e4e11509f36211fe7c13e6f158924f578df2611b
Arthur Andreev <arthur.mailbox@yandex.com>
2018-01-30 15:39:17 +0000

    pair programming code

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,57 @@
+def return_10
+  return 10
+end
+
+def add(first_number, second_number)
+  return first_number + second_number
+end
+
+def subtract(first_number, second_number)
+  return first_number - second_number
+end
+
+def multiply(first_number, second_number)
+  return first_number * second_number
+end
+
+def divide(first_number, second_number)
+  return first_number / second_number
+end
+
+def length_of_string(string)
+  return string.length()
+end
+
+def join_string(string1, string2)
+  return string1 + string2
+end
+
+def add_string_as_number(string1, string2)
+  return string1.to_i + string2.to_i
+end
+
+def number_to_full_month_name(number)
+  case number
+    when 1
+      return "January"
+    when 3
+      return "March"
+    when 9
+      return "September"
+  end
+end
+
+def number_to_short_month_name(number)
+  case number
+    when 1
+      return "Jan"
+    when 3
+      return "Mar"
+    when 9
+      return "Sep"
+  end
+
+  def volume_of_cube(length)
+    return length * 3
+  end
+end
